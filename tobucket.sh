b3989848240ef47f3ea61e68f03c22dbcd79f4b4
glange84 <81751554+glange84@users.noreply.github.com>
2021-04-01 15:39:26 -0300

    Update tobucket.sh

--- tobucket.sh
+++ tobucket.sh
@@ -7,4 +7,4 @@
 # Alterar para o local da instalacao da cli do oci
 ocicli=/root/bin/oci
 
-$ocicli os object put --namespace $1 --bucket-name $2 --file backup-msql.sql.bz2 --no-multipart >> /var/log/$1-$2.log
+$ocicli os object put --namespace $1 --bucket-name $2 --file $3 --no-multipart >> /var/log/$1-$2-$3.log
